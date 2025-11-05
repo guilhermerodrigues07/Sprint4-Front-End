73693e1e13008ded92c6b27fdc44e49f7c45ddca
KX240HZ <kx240hz@gmail.com>
2025-11-05 20:57:43 -0300

    node-modules

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        gold: '#D1B24A',
+        darkbg: '#0f1720',
+      },
+    },
+  },
+  plugins: [],
+}
